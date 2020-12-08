485ec2c332f1d07a64414a5989d3b9dd6ebef2fc
hgn1416@gmail.com <hgn1416@gmail.com>
2020-12-08 09:57:43 +0100

    AuthorsController Created

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        
+    }
+}
